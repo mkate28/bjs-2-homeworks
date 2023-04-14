ea4b278540533bbd9e77d1cafa1bd476a80c85fc
Ekaterina Mordovina <ek.mordovina28@mail.ru>
2023-04-14 17:56:51 +0500

    Third homework uploaded

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.every((item, index) => arr1.length === arr2.length && item === arr2[index]);
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(person => person.gender === gender).reduce((acc, item, index, arr) => {
+		acc += item.age;
+	    if (index === arr.length - 1) {
+	      return acc / arr.length;
+	    }
+	    return acc;
+	}, 0);
+  	return result;
 }
